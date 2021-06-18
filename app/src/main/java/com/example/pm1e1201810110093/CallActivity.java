a804928771b2cc08cb9d84e0abd24a36ecd93718
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-06-18 01:42:13 -0600

    Diseño de pantalla de MainActivity + ListViewActivity + CallActivity

--- app/src/main/java/com/example/pm1e1201810110093/CallActivity.java
+++ app/src/main/java/com/example/pm1e1201810110093/CallActivity.java
@@ -2,7 +2,9 @@ package com.example.pm1e1201810110093;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CallActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+    }
+
+    public void irMainActivity (View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
