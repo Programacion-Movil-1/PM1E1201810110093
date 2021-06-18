7f00e87335c5511d66e47ef1c3e7f8cbd5dd3eb0
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-06-18 01:19:31 -0600

    Diseño de pantalla de MainActivity + ListViewActivity

--- app/src/main/java/com/example/pm1e1201810110093/ListViewActivity.java
+++ app/src/main/java/com/example/pm1e1201810110093/ListViewActivity.java
@@ -2,7 +2,9 @@ package com.example.pm1e1201810110093;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListViewActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ListViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+    }
+
+    public void irMainActivity (View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
